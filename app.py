101b0603c1c7ea2e3f66497730b6a5df5ccd8541
Thiago Cordeiro <thiago6891@gmail.com>
2018-04-26 10:17:20 -0300

    script fully functional

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import csv
+from datetime import datetime
 from html.parser import HTMLParser
 from urllib.request import urlopen
 
@@ -28,10 +30,7 @@ class PageParser(HTMLParser):
             self._data[-1].append(data)
         
     def handle_endtag(self, tag):
-        if tag == 'tr' and len(self._data[-1]) == 0:
-            self._data.pop()
-        
-        if tag == 'tr' and len(self._data) > 0:
+        if tag == 'tr':
             if len(self._data[-1]) == 0:
                 self._data.pop()
             else:
@@ -52,17 +51,51 @@ class PageParser(HTMLParser):
 if __name__ == '__main__':
     symbols = ['QD', 'MSFT', 'GOOG', 'TSLA', 'QQQ', 'GRVY']
 
-    for sym in symbols:
-        url = 'https://finance.yahoo.com/quote/' + sym + '/options?ltr=1'
-        print('Opening', url)
-        response = urlopen(url)
-        print('Decoding HTML...')
-        html = response.read().decode('utf-8')
-        print('Parsing page...')
-        parser = PageParser()
-        parser.feed(html)
+    now = datetime.now()
+    
+    options_file = open('options_' + now.strftime('%Y%m%d') + '.csv', 'w', newline='')
+    errors_file = open('errors_' + now.strftime('%Y%m%d') + '.csv', 'w', newline='')
+    
+    options_writer = csv.writer(options_file)
+    errors_writer = csv.writer(errors_file)
 
-        print('Data for symbol', sym)
-        for d in parser.data:
-            print(d)
-        print(parser.price)
+    options_writer.writerow([
+        'Contract Name',
+        'Last Trade Date',
+        'Strike',
+        'Last Price',
+        'Bid',
+        'Ask',
+        'Change',
+        'Change %',
+        'Volume',
+        'Open Interest',
+        'Implied Volatility',
+        'Stock Price',
+    ])
+    errors_writer.writerow(['Symbol', 'Stock Price/Error'])
+
+    for sym in symbols:
+        try:
+            url = 'https://finance.yahoo.com/quote/' + sym + '/options?ltr=1'
+            
+            print('Opening', url)
+            response = urlopen(url)
+            
+            print('Decoding HTML...')
+            html = response.read().decode('utf-8')
+            
+            print('Parsing page...')
+            parser = PageParser()
+            parser.feed(html)
+
+            if parser.data:
+                for d in parser.data:
+                    options_writer.writerow(d)
+            else:
+                errors_writer.writerow([sym, parser.price])
+        except Exception as e:
+            errors_writer.writerow([sym, e])
+    
+    options_file.close()
+    errors_file.close()
